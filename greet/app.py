b07585ae1846a733dbf71db5236bfa69556c2374
marauder77 <63757565+marauder77@users.noreply.github.com>
2022-04-30 01:02:21 -0700

    completed greet Flask exercices and ran test

--- greet/app.py
+++ greet/app.py
@@ -1,3 +1,15 @@
 from flask import Flask
 
 app = Flask(__name__)
+
+@app.route('/welcome')
+def welcome_page():
+    return "Welcome!"
+
+@app.route('/welcome/home')
+def home_page():
+    return "Welcome home!"
+
+@app.route('/welcome/back')
+def back_page():
+    return "Welcome back!"
