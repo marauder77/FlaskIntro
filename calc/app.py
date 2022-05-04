01c85968ae03dbe66d3f56f1690f38e09bbc2a89
marauder77 <63757565+marauder77@users.noreply.github.com>
2022-05-03 21:17:15 -0700

    updating calc files

--- calc/app.py
+++ calc/app.py
@@ -1,1 +1,40 @@
-# Put your app in here.
+from flask import Flask, request 
+from operations import add, sub, mult, div
+
+app = Flask(__name__)
+
+
+def add(a, b):
+    """Add a and b."""
+    a = int(request.args.get("a"))
+    b = int(request.args.get("b"))
+    result = add(a, b)
+    
+    return str(result)
+
+def sub(a, b):
+    """Substract b from a."""
+
+    a = int(request.args.get("a"))
+    b = int(request.args.get("b"))
+    result = sub(a, b)
+    
+    return str(result)
+
+def mult(a, b):
+    """Multiply a and b."""
+
+    a = int(request.args.get("a"))
+    b = int(request.args.get("b"))
+    result = mult(a, b)
+    return str(result)
+
+
+def div(a, b):
+    """Divide a by b."""
+
+    a = int(request.args.get("a"))
+    b = int(request.args.get("b"))
+    result = div(a, b)
+    
+    return str(result)
